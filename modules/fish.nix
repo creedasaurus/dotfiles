72ddb7b28cb4ad3be704542ea79784ac39aee186
Creed Haymond <creedasaurus@gmail.com>
2021-04-06 09:43:46 -0700

    Adding bass to fish

--- modules/fish.nix
+++ modules/fish.nix
@@ -8,8 +8,21 @@
       la = "exa -lah";
       lt = "exa --tree --level=2 --long";
     };
-  };
+
+    plugins = [
+      {
+        name = "bass";
+        src = pkgs.fetchFromGitHub {
+          owner = "edc";
+          repo = "bass";
+          rev = "2fd3d2157d5271ca3575b13daec975ca4c10577a";
+          sha256 = "fl4/Pgtkojk5AE52wpGDnuLajQxHoVqyphE90IIPYFU=";
+        };
+      }
+    ];
   
+  };
+
   home.packages = with pkgs; [
     exa
     bat
