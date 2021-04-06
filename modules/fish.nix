7a12f775862c0192233b318de22fefe4b2f7afe5
Creed Haymond <creedasaurus@gmail.com>
2021-04-06 10:26:00 -0700

    fixing fish

--- modules/fish.nix
+++ modules/fish.nix
@@ -10,15 +10,7 @@
     };
 
     plugins = [
-      {
-        name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "bass";
-          rev = "2fd3d2157d5271ca3575b13daec975ca4c10577a";
-          sha256 = "fl4/Pgtkojk5AE52wpGDnuLajQxHoVqyphE90IIPYFU=";
-        };
-      }
+      
     ];
   
   };
