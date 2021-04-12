715ed8d0c2c92b3b123c8c201654ea7ceca1ffa7
Creed Haymond <creedasaurus@gmail.com>
2021-04-12 09:37:14 -0700

    Not sure how to have this conditional yet, so I'll put it in a branch

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -1,14 +1,34 @@
-{ config, pkgs, libs, ... }:
+{ config, pkgs, lib, ... }:
 {
   home.stateVersion = "21.05";
-  
+
   nixpkgs.config.allowUnfree = true;
 
-  xdg.enable=true;
-  xdg.mime.enable=true;
-  targets.genericLinux.enable = true;
+  # xdg.enable=true;
+  # xdg.mime.enable=true;
+  # targets.genericLinux.enable = true;
   fonts.fontconfig.enable = true;
-  
+
+  home.activation = {
+    copyApplications = let
+      apps = pkgs.buildEnv {
+        name = "home-manager-applications";
+        paths = config.home.packages;
+        pathsToLink = "/Applications";
+      };
+    in lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      baseDir="$HOME/Applications/Home Manager Apps"
+      if [ -d "$baseDir" ]; then
+        rm -rf "$baseDir"
+      fi
+      mkdir -p "$baseDir"
+      for appFile in ${apps}/Applications/*; do
+        target="$baseDir/$(basename "$appFile")"
+        $DRY_RUN_CMD cp ''${VERBOSE_ARG:+-v} -fHRL "$appFile" "$baseDir"
+        $DRY_RUN_CMD chmod ''${VERBOSE_ARG:+-v} -R +w "$target"
+      done
+    '';
+  };
 
   programs.man.enable = false;
   home.extraOutputsToInstall = [ "man" ];
