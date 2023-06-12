97607d8e1fc90eec869955df94338ea8a5d09658
Creed Haymond <creedasaurus@gmail.com>
2023-06-11 21:23:05 -0600

    Add fzf config after installs

--- .chezmoiscripts/darwin/run_onchange_install-asdf.sh
+++ .chezmoiscripts/darwin/run_onchange_install-asdf.sh
@@ -5,3 +5,7 @@ echo "Installing asdf plugins"
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add python https://github.com/asdf-community/asdf-python
+
+
+echo "Set up fzf - post-install"
+$(brew --prefix)/opt/fzf/install --xdg --key-bindings --completion --no-update-rc
