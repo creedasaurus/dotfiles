28cf2d33a230d80243f95a6aa70f3688e1b768c1
Creed Haymond <creedasaurus@gmail.com>
2023-06-11 21:25:17 -0600

    Add asdf java install

--- .chezmoiscripts/darwin/run_onchange_install-asdf.sh
+++ .chezmoiscripts/darwin/run_onchange_install-asdf.sh
@@ -5,7 +5,7 @@ echo "Installing asdf plugins"
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add python https://github.com/asdf-community/asdf-python
-
+asdf plugin-add java https://github.com/halcyon/asdf-java.git
 
 echo "Set up fzf - post-install"
 $(brew --prefix)/opt/fzf/install --xdg --key-bindings --completion --no-update-rc
