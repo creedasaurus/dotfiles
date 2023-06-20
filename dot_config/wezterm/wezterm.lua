446476ef037c6eb7e24d8dd3b9092a5e35f58f9b
Creed Haymond <creedasaurus@gmail.com>
2023-06-20 09:27:44 -0600

    adding Chezmoi config and update wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,9 +1,18 @@
-
 local wezterm = require 'wezterm'
 local config = {}
 
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- Font type 
 config.font = wezterm.font('JetBrains Mono')
+config.font_size = 12.2
+
+-- Colors
 config.color_scheme = 'Operator Mono Dark'
+
+-- Window frame font style
 config.window_frame = {
 	font = wezterm.font { family = 'JetBrains Mono SemiBold' }
 }
