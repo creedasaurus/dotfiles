65f057b36d729842a3415fad7ffeea69b332123e
Creed Haymond <creedasaurus@gmail.com>
2023-06-21 10:20:05 -0600

    Testing out Zellij and some Catppuccin theme

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -7,15 +7,24 @@ end
 
 -- Font type 
 config.font = wezterm.font('JetBrains Mono')
-config.font_size = 12.2
+config.font_size = 13.0
 
 -- Colors
-config.color_scheme = 'Operator Mono Dark'
+config.color_scheme = 'Catppuccin Mocha'
 
 -- Window frame font style
 config.window_frame = {
 	font = wezterm.font { family = 'JetBrains Mono SemiBold' }
 }
+
+config.window_padding = {
+  left = '0.25cell',
+  right = '0.25cell',
+  top = '0.25cell',
+  bottom = '0.25cell',
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
 
