1e4a55e3fe88dced494c55520d7eec338ef6a276
Creed Haymond <creedasaurus@gmail.com>
2024-02-08 12:59:25 -0700

    fixing gruvbox dark hard contrast

--- dot_config/nvim/lua/plugins/theme.lua
+++ dot_config/nvim/lua/plugins/theme.lua
@@ -1,6 +1,12 @@
 return {
   {
     "ellisonleao/gruvbox.nvim",
+    opts = {
+      contrast = "hard",
+      palette_overrides = {
+        dark0_hard = "#1b1b1b",
+      },
+    },
   },
   -- Configure LazyVim to load gruvbox
   {
