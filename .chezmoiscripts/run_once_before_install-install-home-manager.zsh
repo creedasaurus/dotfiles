bd071d4fee453e556b6b6e0c161a8d73dbffd77c
Creed Haymond <creedasaurus@gmail.com>
2024-02-29 16:02:39 -0700

    run once install home-manager

--- .chezmoiscripts/run_once_before_install-install-home-manager.zsh
+++ .chezmoiscripts/run_once_before_install-install-home-manager.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-if (( $+commands[nix-channel-wow] )); then
-  # nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
-  # nix-channel --update
-  # nix-shell '<home-manager>' -A install
+if (( $+commands[nix-shell] )); then
+  nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
+  nix-channel --update
+  nix-shell '<home-manager>' -A install
 else
   echo "nix-channel does not seem to be available. Please make sure you've installed nix first!"
 fi
