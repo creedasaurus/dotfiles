842163d3a18ccb68e751df3db10c4b24546ea861
Creed Haymond <creedasaurus@gmail.com>
2024-03-04 10:53:28 -0700

    install home-manager if not available; vim filetype comments

--- .chezmoiscripts/run_once_before_install-install-home-manager.zsh
+++ .chezmoiscripts/run_once_before_install-install-home-manager.zsh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh
 
-if (( $+commands[nix-shell] )); then
+if (( $+commands[home-manager] )); then
+  echo "Nix home-manager is already installed"
+else
+  echo "attempting to install home-manager" 
   nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz home-manager
   nix-channel --update
   nix-shell '<home-manager>' -A install
-else
-  echo "nix-channel does not seem to be available. Please make sure you've installed nix first!"
 fi
